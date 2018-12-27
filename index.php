20fe3a8fd27ef19e8e58110b3a0d8f70159c9792
Lagrik <rain.demorser@gmail.com>
2018-12-27 13:48:38 +0300

    php-files

--- index.php
+++ index.php
@@ -1,228 +1,51 @@
+<?php 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+  if(isset($_POST['Name'])) $Name = $_POST['Name'];
+  if (isset($_POST['Email'])) $Email = $_POST['Email'];
+  if(isset($_POST['Phone'])) $Phone = $_POST['Phone'];
+  if (isset($_POST['Message'])) $Message = $_POST['Message'];
 
-	<link href="style/style.css" rel="stylesheet" type="text/css">
-	<link href="https://fonts.googleapis.com/css?family=Mali" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Patua+One" rel="stylesheet"> 
-	<link href="jquery-ui-1.12.1.custom/jquery-ui.css" rel="stylesheet" type="text/css">
-	<link href="jquery-ui.css" rel="stylesheet" type="text/css">
+  $host = '127.0.0.1';
+  $db   = 'MaketBase';
+  $user = 'root';
+  $pass = '';
+  $charset = 'utf8';
 
-	<title>PHP-y-y-y-y</title>
-</head>
+  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+  $opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $opt);
 
-<body>
-	<style>
-	h1{
-		color: orange;
-	}
-</style>
 
-<?php
-
-?>
-<?php
-if ($a == 5):
-	echo "a равно 5";
-	echo "...";
-elseif ($a == 6):
-	echo "a равно 6";
-	echo "!!!";
-else:
-	echo "a не равно ни 5 ни 6";
-endif;
-?>
-
-<?php
-echo "<br/>";
-$a = 5;
-$b = 5;
-if ($a > $b){
-	echo "a больше b";
+$pattern_Name = "/[a-zA-Z]{3,50}/";
+$pattern_Email = "/[a-zA-Z]{1,50}/";
+$pattern_Phone = "/\d{11,11}/";
+if(!preg_match($pattern_Name, $Name)){
+echo 'It`s not working';
 }
 else{
-	echo "<h1>a меньше b</h1>";
+if(!preg_match($pattern_Email, $Email)){
+echo 'It`s not working';
 }
-echo "<br/>";
-$e3 = 4;
-$d3 = 4;
-if ($a3 > $b3) {
-	echo "a больше, чем b";
-} elseif ($a3 == $b3) {
-	echo "a равен b";
-} else {
-	echo "a меньше, чем b";
+else{
+if(!preg_match($pattern_Phone, $Phone)){
+echo 'It`s not working';
 }
-?>
+else{
 
-
-
-<?php
-echo "<br/>";
-$i = 1;
-while ($i <= 10) {
-	echo $i++;
+$mysql = "INSERT INTO  ContactUs (Name,Email,Phone,Message) VALUES(:Name,:Email,:Phone,:Message)";
+$q = $pdo->prepare($mysql);
+$q->execute(array(':Name'=>$Name,
+                  ':Email'=>$Email,
+                  ':Phone'=>$Phone,
+                  ':Message'=>$Message));
 }
-?>
-
-<?php
-echo "<br/>";
-for ($i = 90; $i <= 100; $i++) {
-	echo "<br/>";
-	echo $i;
 }
-?>
-
-<?php
-echo "<br/>";
-$i = 1;
-do {
-	echo "<br/>";
-	echo $i;
-	$i++;
-} while ($i <= 10);
-
-?>
-
-<?php
-$arr = array(1,2,3,4,5,6,7,8,9,10);
-echo "<br/>";
-foreach ($arr as &$value) {
-	echo "<br/>";
-	echo $value;
 }
-?>
-
-<?php
-$i = 1991;
-while ($i <= 1991) {?>
-	<div style="width: 100px; height: 60px; background-color: red; color: white;"><h2>&#9773;</h2></div>
-	<?php echo $i;?>
-
-	<?php
-	$i++;
-}?>
 
 
-<?php
-$arr = array(41, 22, 46, 7, 0, -2, -3, 41, 0, 0, -17, -1);
-
-$kol = count($arr);
-
-$min = $max = $arr[0]; 
-$index_min = $index_max = 0; 
-
-for ($i = 1; $i < $kol; $i++) { 
-	if ($arr[$i] > $max) { 
-		$index_max = $i; 
-		$max = $arr[$i]; 
-	} else 
-	if ($arr[$i] < $min) { 
-		$index_min = $i; 
-		$min = $arr[$i]; 
-	} 
-} 
-
-echo "<br/>";
-echo "<br/>";
-
-
-echo "Максимальное число = $max<br>"; 
-echo "Минимальное число = $min<br>"; 
-
-?>
-
-<?php 
-
-$arr = array(41, 22, 46, 7, 0, -2, -3, 41, 0, 0,  -17, -1);
-
-$kol = count($arr);
-
-
-$index_o = 0; 
-
-for ($i = 0; $i < $kol; $i++) { 
-	if ($arr[$i] >0) { 
-		
-		$index_o = $index_o+$arr[$i];
-		 
-	}
-} 
-
-echo "<br/>";
-
-echo "Сумма положительных чисел = $index_o  ";
-
-?>
-<?php 
-echo "<br/>";
-for ($i=0; $i<9; $i++) { 
-	echo "<br/>";
- 	for ($j=0; $j<6; $j++){
- 		echo $mas[i][j] = rand (15,35)." | ";
- 						  }				  
- 					} 	
  ?>
-
-
-<?php 
-echo "<br/>";
-for ($i=0; $i<9; $i++) { 
-	echo "<br/>";
- 	for ($j=0; $j<6; $j++){
- 		echo $mas[i][j] = rand (15,35)." | ";
- 		if ($i==2){
- 			$sklad = $sklad+$mas[i][j];
- 			      }
- 						  }
- 					   }	
- 		echo "<br/>";			  
- 		echo $sklad;			
-
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- 
-<?php 
-echo "<br/>";
-for ($i=0; $i<10; $i++) { 
-	echo "<br/>";
- 	for ($j=0; $j<10; $j++){
- 		echo $mas[i][j] = rand (0,0)." | ";
- 						  }
- 					} 
- 	echo $toch1 [5][9] = 1;
- ?> -->
-
-
-
-
-
-
-
-
-
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>  
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<script type="text/javascript" src="jquery-ui-1.12.1.custom/jquery-ui.js"></script>
-<script type="text/javascript" src="jquery-ui.js"></script>
-<script src="js/script.js"></script> 
-</body>
-</html>
 
